e45a5dcf02ee423ad0de515846b4b9699478f777
ph <robotnaoborot@gmail.com>
2019-12-01 10:22:42 +0300

    fuuu

--- unrest_framework/management/commands/export_to_mongo.py
+++ unrest_framework/management/commands/export_to_mongo.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from unrest_framework import views
+import pymongo
+import logging
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -9,6 +12,11 @@ class Command(BaseCommand):
         parser.add_argument('app', nargs="?", type=str)
 
     def handle(self, *args, **options):
+        url, db = settings.MONGO_URL.rstrip('/').rsplit('/', 1)
+        db = pymongo.MongoClient(url)[db]
+
         for view in views.get_analyzed_views(args and args[0]):
-            db[collection].insert_many(view.get_data())
+            data = view.get_data()
+            logging.info(f'Inserting {len(data)} objects into collection {view.view_name}')
+            db[view.view_name].insert_many()
 
